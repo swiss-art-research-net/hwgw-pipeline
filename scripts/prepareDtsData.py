824de702d7a42fd48a1f85b766ee82aac5b0a410
jbatin <josip.batin@gmail.com>
2026-04-15 16:05:46 +0200

    add license and series metadata, add chapter titles as cideData

--- scripts/prepareDtsData.py
+++ scripts/prepareDtsData.py
@@ -102,6 +102,36 @@ def extract_languages(tei_file):
             result.append(l.attrib["ident"])
     return result
 
+def extract_license(tei_file):
+    tree = parse_xml(tei_file)
+    # Try to get the text content of <availability>
+    availability = tree.xpath("/tei:TEI/tei:teiHeader/tei:fileDesc/tei:publicationStmt/tei:availability", namespaces=NS)
+    if availability and availability[0].text:
+        return [availability[0].text.strip()]
+    # If availability has a <p> child, get its text
+    p = tree.xpath("/tei:TEI/tei:teiHeader/tei:fileDesc/tei:publicationStmt/tei:availability/p", namespaces=NS)
+    if p and p[0].text:
+        return [p[0].text.strip()]
+    return []
+
+def extract_series(tei_file):
+    tree = parse_xml(tei_file)
+    # Get all <title> under <seriesStmt>
+    series_titles = tree.xpath("/tei:TEI/tei:teiHeader/tei:fileDesc/tei:seriesStmt/tei:title", namespaces=NS)
+    return [s.text.strip() for s in series_titles if s.text]
+
+# def extract_chapter_titles(tei_file):
+#     tree = parse_xml(tei_file)
+#     # Find all <div type="chapter">
+#     chapters = tree.xpath('//tei:body/tei:div[@type="chapter"]', namespaces=NS)
+#     titles = []
+#     for ch in chapters:
+#         head = ch.find('tei:head', namespaces=NS)
+#         if head is not None and 'n' in head.attrib:
+#             n_val = head.attrib['n'].strip()
+#             if n_val:
+#                 titles.append(n_val)
+#     return titles
 
 # --- Dynamic refsDecl/citeStructure generation ---
 def analyze_body_structure_and_build_citestructure(tree):
@@ -129,11 +159,14 @@ def analyze_body_structure_and_build_citestructure(tree):
             else:
                 match = f"div[@type='chapter']"
                 cs = etree.Element('citeStructure', unit=typ, match=match, use='@xml:id', delim=":")
+            # Inject <citeData property="http://purl.org/dc/terms/title" use="head/@n"/> for chapters
+            cite_data = etree.Element('citeData', property="http://purl.org/dc/terms/title", use="head/@n")
+            cs.append(cite_data)
             child = deepest_div_path(chapter_div, is_top_level=False)
             if child is not None:
                 cs.append(child)
             return cs
-        # Otherwise, pick the first div
+        # Otherwise, pick the first div (subchapter or other div)
         if divs:
             d = divs[0]
             typ = d.attrib.get('type', 'div')
@@ -143,6 +176,9 @@ def analyze_body_structure_and_build_citestructure(tree):
             else:
                 match = f"div[@type='{typ}']"
                 cs = etree.Element('citeStructure', unit=typ, match=match, use='@xml:id', delim=":")
+            # Inject <citeData property="http://purl.org/dc/terms/title" use="head/@n"/> for all divs
+            cite_data = etree.Element('citeData', property="http://purl.org/dc/terms/title", use="head/@n")
+            cs.append(cite_data)
             child = deepest_div_path(d, is_top_level=False)
             if child is not None:
                 cs.append(child)
@@ -295,7 +331,6 @@ def create_subcollection(catalog_root, source_folder, output_root, base_identifi
         dublin = etree.SubElement(resource, "dublinCore")
 
         for author in extract_authors(tei_file):
-            # creator_el = etree.SubElement(dublin, "creator")
             creator_el = etree.SubElement(dublin, "creator", xmlns="http://purl.org/dc/terms/")
             creator_el.text = author
 
@@ -314,6 +349,15 @@ def create_subcollection(catalog_root, source_folder, output_root, base_identifi
         for lang in extract_languages(tei_file):
             lang_el = etree.SubElement(dublin, "language", xmlns="http://purl.org/dc/terms/")
             lang_el.text = lang
+
+        for license_text in extract_license(tei_file):
+            rights_el = etree.SubElement(dublin, "rights", xmlns="http://purl.org/dc/terms/")
+            rights_el.text = license_text
+
+        for series_title in extract_series(tei_file):
+            ispartof_el = etree.SubElement(dublin, "isPartOf", xmlns="http://purl.org/dc/terms/")
+            ispartof_el.text = series_title
+
 
     tree = etree.ElementTree(subcollection)
 
